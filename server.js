ebf6763ea382b08804da24ee852b4429545ec7ed
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-10-09 01:30:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"], // Allow frontend origin
+  origin: ["https://zenpulse-ui.vercel.app"], // Allow frontend origin
   credentials: true // Allow credentials if needed
 }));
 app.use(express.json());
