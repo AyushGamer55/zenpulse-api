824fd848568b3f43e3ad077b68dbbd3a1f049225
AyushGamer55 <ayusharyankumar55@gmail.com>
2025-10-08 19:20:00 +0530

    Update db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL.includes("neon.tech") ? { rejectUnauthorized: false } : false,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
